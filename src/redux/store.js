848ef0d988589b5584b0f9984d11243a574266c0
Chaitanya <chaitu.vsh@gmail.com>
2021-02-21 23:24:24 +0530

    React Redux Dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
 import rootReducer from "../redux/rootReducer";
 
 const middleWare = applyMiddleware(logger)
-const store = createStore(rootReducer,middleWare);
+const store = createStore(rootReducer, compose(
+    middleWare,
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  ));
 
 export default store;
+// https://github.com/zalmoxisus/redux-devtools-extension/issues/126
